a5fb2c74192b45e4a5d663e4917546c93b15b5b5
Carla Sotomayor <csotomayor@denimgroup.com>
2017-05-15 18:57:14 -0500

    adjusting to use manage.py to find settings

--- threadfix-ham/src/main/java/com/denimgroup/threadfix/framework/impl/django/DjangoEndpointGenerator.java
+++ threadfix-ham/src/main/java/com/denimgroup/threadfix/framework/impl/django/DjangoEndpointGenerator.java
@@ -5,7 +5,6 @@ import com.denimgroup.threadfix.framework.engine.full.EndpointGenerator;
 import com.denimgroup.threadfix.framework.util.EventBasedTokenizer;
 import com.denimgroup.threadfix.framework.util.EventBasedTokenizerRunner;
 import com.denimgroup.threadfix.logging.SanitizedLogger;
-import com.sun.xml.internal.fastinfoset.stax.events.EventBase;
 
 import javax.annotation.Nonnull;
 import java.io.File;
@@ -28,20 +27,28 @@ public class DjangoEndpointGenerator implements EndpointGenerator{
         assert rootDirectory.exists() : "Root file did not exist.";
         assert rootDirectory.isDirectory() : "Root file was not a directory.";
 
-        File settingsFile = new File(rootDirectory, "/settings.py");
-        if (!settingsFile.exists()) {
-            LOG.error("File /config/routes.rb not found. Exiting.");
-            return;
+        File manageFile = new File(rootDirectory, "manage.py");
+        assert manageFile.exists() : "manage.py does not exist in root directory";
+        SettingsFinder settingsFinder = new SettingsFinder();
+        EventBasedTokenizerRunner.run(manageFile, settingsFinder);
+
+        File settingsFile = settingsFinder.getSettings(rootDirectory.getPath());
+        assert settingsFile.exists() : "Settings file not found";
+        UrlFileFinder urlFileFinder = new UrlFileFinder();
+
+        if (settingsFile.isDirectory()) {
+            for (File file : settingsFile.listFiles()) {
+                EventBasedTokenizerRunner.run(file, urlFileFinder);
+                if (!urlFileFinder.shouldContinue) break;
+            }
+        } else {
+            EventBasedTokenizerRunner.run(settingsFile, urlFileFinder);
         }
+        assert !urlFileFinder.getUrlFile().isEmpty() : "Root URL file setting does not exist.";
 
-        UrlFileFinder finder = new UrlFileFinder();
-        EventBasedTokenizerRunner.run(settingsFile, finder);
-        assert !finder.getUrlFile().isEmpty() : "Root URL file setting does not exist.";
-
-        File routesFile = new File(rootDirectory, finder.getUrlFile());
+        File routesFile = new File(rootDirectory, urlFileFinder.getUrlFile());
         DjangoRouteParser routeParser = new DjangoRouteParser();
         EventBasedTokenizerRunner.run(routesFile, routeParser);
-
 
     }
 
@@ -54,6 +61,32 @@ public class DjangoEndpointGenerator implements EndpointGenerator{
     @Override
     public Iterator<Endpoint> iterator() {
         return endpoints.iterator();
+    }
+
+    static class SettingsFinder implements EventBasedTokenizer {
+
+        String settingsLocation = "";
+        boolean shouldContinue = true, foundSettingsLocation = false;
+
+        public File getSettings(String rootDirectory) { return DjangoPathCleaner.buildPath(rootDirectory, settingsLocation); }
+
+        @Override
+        public boolean shouldContinue() {
+            return shouldContinue;
+        }
+
+        @Override
+        public void processToken(int type, int lineNumber, String stringValue) {
+            if (type == StreamTokenizer.TT_WORD && stringValue.equals("DJANGO_SETTINGS_MODULE")) {
+                foundSettingsLocation = true;
+            } else if (foundSettingsLocation == true && type == StreamTokenizer.TT_WORD) {
+                settingsLocation = DjangoPathCleaner.cleanStringFromCode(stringValue);
+            }
+
+            if (!settingsLocation.isEmpty()) {
+                shouldContinue = false;
+            }
+        }
     }
 
     static class UrlFileFinder implements EventBasedTokenizer {
@@ -70,6 +103,7 @@ public class DjangoEndpointGenerator implements EndpointGenerator{
 
         @Override
         public void processToken(int type, int lineNumber, String stringValue) {
+
             if (type == StreamTokenizer.TT_WORD && stringValue.equals("ROOT_URLCONF")) {
                 foundURLSetting = true;
             } else if (foundURLSetting == true && type == StreamTokenizer.TT_WORD) {
--- threadfix-ham/src/main/java/com/denimgroup/threadfix/framework/impl/django/DjangoPathCleaner.java
+++ threadfix-ham/src/main/java/com/denimgroup/threadfix/framework/impl/django/DjangoPathCleaner.java
@@ -1,56 +1,32 @@
 package com.denimgroup.threadfix.framework.impl.django;
 
-import com.denimgroup.threadfix.framework.engine.cleaner.PathCleaner;
-import com.denimgroup.threadfix.framework.engine.full.EndpointGenerator;
+import com.denimgroup.threadfix.framework.engine.cleaner.DefaultPathCleaner;
+import com.denimgroup.threadfix.framework.engine.partial.PartialMapping;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import java.io.File;
+import java.util.List;
 
 /**
  * Created by csotomayor on 5/15/2017.
  */
-public class DjangoPathCleaner implements PathCleaner {
+public class DjangoPathCleaner extends DefaultPathCleaner {
+
+    public DjangoPathCleaner(List<PartialMapping> partialMappings) {
+        super(partialMappings);
+    }
+
+    public static File buildPath(String root, String input) {
+        StringBuilder builder = new StringBuilder(64);
+        builder.append(root).append("/").append(input);
+        File file = new File(builder.toString());
+        if (file.isDirectory()) {
+            return file;
+        } else {
+            return new File(builder.append(".py").toString());
+        }
+    }
 
     public static String cleanStringFromCode(String input) {
-        return "/" + input.replace('.', '/').concat(".py");
-    }
-
-    public static String cleanStringFromScan(String input) {
-        return input;
-    }
-
-    @Nullable
-    @Override
-    public String cleanStaticPath(@Nonnull String filePath) {
-        return filePath;
-    }
-
-    @Nullable
-    @Override
-    public String cleanDynamicPath(@Nonnull String urlPath) {
-        return urlPath;
-    }
-
-    @Nullable
-    @Override
-    public String getDynamicPathFromStaticPath(@Nonnull String filePath) {
-        return filePath;
-    }
-
-    @Nullable
-    @Override
-    public String getDynamicRoot() {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public String getStaticRoot() {
-        return null;
-    }
-
-    @Override
-    public void setEndpointGenerator(EndpointGenerator generator) {
-
+        return input.replace('.', '/').concat(".py");
     }
 }
