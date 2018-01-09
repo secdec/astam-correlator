2300897517814c9e0e6f129a9101a2df70722a8e
Tyler Camp <tylerc@avi.com>
2018-01-09 16:30:49 -0500

    Updated licensing headers for threadfix-astam project

--- threadfix-astam/src/main/java/com/denimgroup/threadfix/cds/service/integration/AstamApplicationImporterImpl.java
+++ threadfix-astam/src/main/java/com/denimgroup/threadfix/cds/service/integration/AstamApplicationImporterImpl.java
@@ -1,21 +1,25 @@
-// Copyright 2017 Secure Decisions, a division of Applied Visions, Inc.
+////////////////////////////////////////////////////////////////////////
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//     Copyright (C) 2017 Applied Visions - http://securedecisions.com
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
 //
-// This material is based on research sponsored by the Department of Homeland
-// Security (DHS) Science and Technology Directorate, Cyber Security Division
-// (DHS S&T/CSD) via contract number HHSP233201600058C.
-
+//     This material is based on research sponsored by the Department of Homeland
+//     Security (DHS) Science and Technology Directorate, Cyber Security Division
+//     (DHS S&T/CSD) via contract number HHSP233201600058C.
+//
+//     Contributor(s):
+//              Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
 package com.denimgroup.threadfix.cds.service.integration;
 
 
