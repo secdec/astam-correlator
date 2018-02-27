45af448a093eff6f83b3ef7f5b7d6570810e67b0
MDeLetto <matthew.deletto@avi.com>
2018-02-27 13:22:24 -0500

    Add port selection controls to Code PT configuration

--- threadfix-scanner-plugin/burp/src/main/java/burp/custombutton/EndpointsButton.java
+++ threadfix-scanner-plugin/burp/src/main/java/burp/custombutton/EndpointsButton.java
@@ -127,8 +127,6 @@ public abstract class EndpointsButton extends JButton {
                     }
                         else
                             {
-                                JOptionPane.showMessageDialog(view, "A target URL configuration is required to populate the site map with the detected endpoints",
-                                        "Warning", JOptionPane.WARNING_MESSAGE);
                                 makeReqs = false;
                             }
                     }
