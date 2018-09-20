3b42d5b025b189fd65a10ed59b937516229b6406
Tyler Camp <tylerc@avi.com>
2018-09-20 16:07:58 -0400

    Update unit tests for new changes

--- threadfix-ham/src/test/java/com/denimgroup/threadfix/framework/impl/dotNet/DotNetControllerParserTests.java
+++ threadfix-ham/src/test/java/com/denimgroup/threadfix/framework/impl/dotNet/DotNetControllerParserTests.java
@@ -24,14 +24,22 @@
 package com.denimgroup.threadfix.framework.impl.dotNet;
 
 import com.denimgroup.threadfix.framework.ResourceManager;
+import com.denimgroup.threadfix.framework.impl.dotNet.actionMappingGenerators.AspActionGenerator;
+import com.denimgroup.threadfix.framework.impl.dotNet.actionMappingGenerators.AspStandardApiActionGenerator;
+import com.denimgroup.threadfix.framework.impl.dotNet.actionMappingGenerators.AspStandardMvcActionGenerator;
+import com.denimgroup.threadfix.framework.impl.dotNet.classDefinitions.CSharpClass;
+import com.denimgroup.threadfix.framework.impl.dotNet.classParsers.CSharpFileParser;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static com.denimgroup.threadfix.CollectionUtils.list;
+import static com.denimgroup.threadfix.CollectionUtils.map;
 
 /**
  * Created by mac on 6/11/14.
@@ -41,134 +49,160 @@ public class DotNetControllerParserTests {
     //  Commented these tests for now since the DotNetControllerParser is now superceded by
     //  C# class parsing and DotNetMappings*Generators
 
-//    @Nonnull
-//    private DotNetControllerMappings getControllerMappings(String fileName) {
-//        return DotNetControllerParser.parse(ResourceManager.getDotNetMvcFile(fileName)).get(0);
-//    }
+    @Nonnull
+    private DotNetControllerMappings getMvcStandardControllerMappings(String fileName) {
+        List<CSharpClass> fileClasses = CSharpFileParser.parse(ResourceManager.getDotNetMvcFile(fileName));
+        Map<String, RouteParameterMap> fileParams = map(fileName, new RouteParameterMap());
+
+        CSharpClass mainClass = fileClasses.get(0);
+        if (!mainClass.getBaseTypes().contains("Controller")) {
+            mainClass.getBaseTypes().add("Controller");
+        }
+
+        AspActionGenerator actionGenerator = new AspStandardMvcActionGenerator(fileClasses, fileParams);
+        return actionGenerator.generate().get(0);
+    }
+
+    @Nonnull
+    private DotNetControllerMappings getApiStandardControllerMappings(String fileName) {
+        List<CSharpClass> fileClasses = CSharpFileParser.parse(ResourceManager.getDotNetMvcFile(fileName));
+        Map<String, RouteParameterMap> fileParams = map(fileName, new RouteParameterMap());
+
+        CSharpClass mainClass = fileClasses.get(0);
+        if (!mainClass.getBaseTypes().contains("ApiController")) {
+            mainClass.getBaseTypes().add("ApiController");
+        }
+
+        AspActionGenerator actionGenerator = new AspStandardApiActionGenerator(fileClasses, fileParams);
+        return actionGenerator.generate().get(0);
+    }
+
+    @Test
+    public void testBasicController() {
+        DotNetControllerMappings mappings = getMvcStandardControllerMappings("ChatController.cs");
+
+        assert mappings.getControllerName() != null :
+                "Controller name was null.";
+        assert mappings.getControllerName().equals("Chat") :
+                "Controller name was " + mappings.getControllerName() + " but should have been Chat.";
+        assert mappings.getActions().size() == 1 :
+                "The size was " + mappings.getActions().size() + " instead of 1.";
+        assert mappings.getActionForNameAndMethod("Index", "GET") != null :
+                "Mappings didn't contain Index. They had " + mappings.getActions().iterator().next();
+    }
 //
-//    @Test
-//    public void testBasicController() {
-//        DotNetControllerMappings mappings = getControllerMappings("ChatController.cs");
-//
-//        assert mappings.getControllerName() != null :
-//                "Controller name was null.";
-//        assert mappings.getControllerName().equals("Chat") :
-//                "Controller name was " + mappings.getControllerName() + " but should have been Chat.";
-//        assert mappings.getActions().size() == 1 :
-//                "The size was " + mappings.getActions().size() + " instead of 1.";
-//        assert mappings.getActionForNameAndMethod("Index", "GET") != null :
-//                "Mappings didn't contain Index. They had " + mappings.getActions().iterator().next();
-//    }
-//
-//    @Test
-//    public void testControllerWithPostAttribute() {
-//        DotNetControllerMappings mappings = getControllerMappings("AttributesController.cs");
-//
-//        assert mappings.getControllerName() != null :
-//                "Controller name was null.";
-//        assert mappings.getControllerName().equals("Account") :
-//                "Controller name was " + mappings.getControllerName() + " but should have been Account.";
-//        assert mappings.getActionForNameAndMethod("Login", "POST") != null :
-//                "Mappings didn't contain Login with POST.";
-//    }
-//
-//    @Test
-//    public void testRestController() {
-//        DotNetControllerMappings mappings = getControllerMappings("RestController.cs");
-//
-//        assert mappings.getControllerName() != null :
-//                "Controller name was null.";
-//        assert mappings.getControllerName().equals("Students") :
-//                "Controller name was " + mappings.getControllerName() + " but should have been Students.";
-//        assert mappings.getActionForNameAndMethod("Get", "GET") != null :
-//                "Mappings didn't contain Get with GET.";
-//    }
-//
-//    @Test
-//    public void testAttributesControllerActionSizeAndMethods() {
-//        DotNetControllerMappings mappings = getControllerMappings("AttributesController.cs");
-//
-//        List<String> expectedActions = Arrays.asList(
-//                "Login",
-//                "Login",
-//                "LogOff",
-//                "Register",
-//                "Register",
-//                "Disassociate",
-//                "Manage",
-//                "Manage",
-//                "ExternalLogin",
-//                "ExternalLoginCallback",
-//                "ExternalLoginConfirmation",
-//                "ExternalLoginFailure",
-//                "ExternalLoginsList",
-//                "RemoveExternalLogins"
-//        ), expectedMethods = Arrays.asList(
-//                "GET",
-//                "POST",
-//                "POST",
-//                "GET",
-//                "POST",
-//                "POST",
-//                "GET",
-//                "POST",
-//                "POST",
-//                "GET",
-//                "POST",
-//                "GET",
-//                "GET",
-//                "GET"
-//        ), missing = list(), extra = list();
-//
-//        assert expectedActions.size() == expectedMethods.size() :
-//                "Expected actions and methods didn't match up.";
-//        assert mappings.getControllerName() != null :
-//                "Controller name was null.";
-//        assert mappings.getControllerName().equals("Account") :
-//                "Controller name was " + mappings.getControllerName() + " but should have been Account.";
-//
-//
-//        for (int i = 0; i < expectedActions.size(); i++) {
-//            if (mappings.getActionForNameAndMethod(expectedActions.get(i), expectedMethods.get(i)) == null) {
-//                missing.add(expectedActions.get(i) + " " + expectedMethods.get(i));
-//            }
-//        }
-//
-//        for (Action action : mappings.getActions()) {
-//            if (!expectedActions.contains(action.name)) {
-//                extra.add(action.name + " " + action.getMethods());
-//            }
-//        }
-//
-//        if (!missing.isEmpty()) {
-//            System.out.println("Controller is missing methods : " + missing);
-//        }
-//
-//        if (!extra.isEmpty()) {
-//            System.out.println("Controller has extra methods : " + extra);
-//        }
-//
-//        assert missing.isEmpty() && extra.isEmpty() : "Wrong number of methods. See above logs.";
-//
-//        assert mappings.getActions().size() == 14 :
-//                "The size was " + mappings.getActions().size() + " instead of 14.";
-//
-//    }
-//
-//    @Test
-//    public void testParameterParsing() {
-//        DotNetControllerMappings mappings = getControllerMappings("InstructorController.cs");
-//
-//        Action targetAction = mappings.getActionForNameAndMethod("Edit", "GET");
-//
-//        assert targetAction != null : "Edit action was null. Can't continue.";
-//
-//        assert targetAction.parameters.keySet().contains("id") : "Parameters didn't contain id.";
-//    }
-//
+    @Test
+    public void testControllerWithPostAttribute() {
+        DotNetControllerMappings mappings = getMvcStandardControllerMappings("AttributesController.cs");
+
+        assert mappings.getControllerName() != null :
+                "Controller name was null.";
+        assert mappings.getControllerName().equals("Account") :
+                "Controller name was " + mappings.getControllerName() + " but should have been Account.";
+        assert mappings.getActionForNameAndMethod("Login", "POST") != null :
+                "Mappings didn't contain Login with POST.";
+    }
+
+    @Test
+    public void testRestController() {
+        DotNetControllerMappings mappings = getApiStandardControllerMappings("RestController.cs");
+
+        assert mappings.getControllerName() != null :
+                "Controller name was null.";
+        assert mappings.getControllerName().equals("Students") :
+                "Controller name was " + mappings.getControllerName() + " but should have been Students.";
+        assert mappings.getActionForNameAndMethod("Get", "GET") != null :
+                "Mappings didn't contain Get with GET.";
+    }
+
+    @Test
+    public void testAttributesControllerActionSizeAndMethods() {
+        DotNetControllerMappings mappings = getMvcStandardControllerMappings("AttributesController.cs");
+
+        List<String> expectedActions = Arrays.asList(
+                "Login",
+                "Login",
+                "LogOff",
+                "Register",
+                "Register",
+                "Disassociate",
+                "Manage",
+                "Manage",
+                "ExternalLogin",
+                "ExternalLoginCallback",
+                "ExternalLoginConfirmation",
+                "ExternalLoginFailure",
+                "ExternalLoginsList",
+                "RemoveExternalLogins"
+        ), expectedMethods = Arrays.asList(
+                "GET",
+                "POST",
+                "POST",
+                "GET",
+                "POST",
+                "POST",
+                "GET",
+                "POST",
+                "POST",
+                "GET",
+                "POST",
+                "GET",
+                "GET",
+                "GET"
+        ), missing = list(), extra = list();
+
+        assert expectedActions.size() == expectedMethods.size() :
+                "Expected actions and methods didn't match up.";
+        assert mappings.getControllerName() != null :
+                "Controller name was null.";
+        assert mappings.getControllerName().equals("Account") :
+                "Controller name was " + mappings.getControllerName() + " but should have been Account.";
+
+
+        for (int i = 0; i < expectedActions.size(); i++) {
+            if (mappings.getActionForNameAndMethod(expectedActions.get(i), expectedMethods.get(i)) == null) {
+                missing.add(expectedActions.get(i) + " " + expectedMethods.get(i));
+            }
+        }
+
+        for (Action action : mappings.getActions()) {
+            if (!expectedActions.contains(action.name)) {
+                extra.add(action.name + " " + action.getMethods());
+            }
+        }
+
+        if (!missing.isEmpty()) {
+            System.out.println("Controller is missing methods : " + missing);
+        }
+
+        if (!extra.isEmpty()) {
+            System.out.println("Controller has extra methods : " + extra);
+        }
+
+        assert missing.isEmpty() && extra.isEmpty() : "Wrong number of methods. See above logs.";
+
+        assert mappings.getActions().size() == 14 :
+                "The size was " + mappings.getActions().size() + " instead of 14.";
+
+    }
+
+    @Test
+    public void testParameterParsing() {
+        DotNetControllerMappings mappings = getMvcStandardControllerMappings("InstructorController.cs");
+
+        Action targetAction = mappings.getActionForNameAndMethod("Edit", "GET");
+
+        assert targetAction != null : "Edit action was null. Can't continue.";
+
+        assert targetAction.parameters.keySet().contains("id") : "Parameters didn't contain id.";
+    }
+
+    //  Keep this left out for now since Bind(Include="...") now requires the proper model definition
+    //  and won't directly use those values as parameters; these test files don't have the appropriate
+    //  model definitions
 //    @Test
 //    public void testBindIncludeSettings() {
-//        DotNetControllerMappings mappings = getControllerMappings("BindingController.cs");
+//        DotNetControllerMappings mappings = getMvcStandardControllerMappings("BindingController.cs");
 //
 //        Action targetAction = mappings.getActionForNameAndMethod("Edit", "POST");
 //
@@ -176,26 +210,26 @@ public class DotNetControllerParserTests {
 //
 //        assert targetAction.parameters.keySet().contains("ID") : "Parameters didn't contain id.";
 //    }
-//
-//    @Test
-//    public void testDefaultValuesParsing() {
-//        DotNetControllerMappings mappings = getControllerMappings("DefaultParametersController.cs");
-//
-//        assert mappings.getActions().size() == 1 :
-//                "Had " + mappings.getActions().size() + " actions, should have had 1.";
-//
-//        Set<String> parameters = mappings.getActions().get(0).parameters.keySet();
-//
-//        assert parameters.contains("id") :
-//                "Parameters didn't contain id but should have: " + parameters;
-//        assert parameters.contains("type") :
-//                "Parameters didn't contain type but should have: " + parameters;
-//        assert parameters.contains("expires") :
-//                "Parameters didn't contain expires but should have: " + parameters;
-//
-//        assert !parameters.contains("null") :
-//                "Parameters contained null but shouldn't have: " + parameters;
-//        assert parameters.size() == 3 :
-//                "Size should have been 3 but was " + parameters;
-//    }
+
+    @Test
+    public void testDefaultValuesParsing() {
+        DotNetControllerMappings mappings = getMvcStandardControllerMappings("DefaultParametersController.cs");
+
+        assert mappings.getActions().size() == 1 :
+                "Had " + mappings.getActions().size() + " actions, should have had 1.";
+
+        Set<String> parameters = mappings.getActions().get(0).parameters.keySet();
+
+        assert parameters.contains("id") :
+                "Parameters didn't contain id but should have: " + parameters;
+        assert parameters.contains("type") :
+                "Parameters didn't contain type but should have: " + parameters;
+        assert parameters.contains("expires") :
+                "Parameters didn't contain expires but should have: " + parameters;
+
+        assert !parameters.contains("null") :
+                "Parameters contained null but shouldn't have: " + parameters;
+        assert parameters.size() == 3 :
+                "Size should have been 3 but was " + parameters;
+    }
 }
--- threadfix-ham/src/test/java/com/denimgroup/threadfix/framework/impl/dotNet/DotNetEndpointGeneratorTests.java
+++ threadfix-ham/src/test/java/com/denimgroup/threadfix/framework/impl/dotNet/DotNetEndpointGeneratorTests.java
@@ -25,40 +25,63 @@ package com.denimgroup.threadfix.framework.impl.dotNet;
 
 import com.denimgroup.threadfix.data.interfaces.Endpoint;
 import com.denimgroup.threadfix.framework.ResourceManager;
+import com.denimgroup.threadfix.framework.impl.dotNet.actionMappingGenerators.AspActionGenerator;
+import com.denimgroup.threadfix.framework.impl.dotNet.actionMappingGenerators.AspStandardMvcActionGenerator;
 import com.denimgroup.threadfix.framework.impl.dotNet.classDefinitions.CSharpClass;
+import com.denimgroup.threadfix.framework.impl.dotNet.classParsers.CSharpFileParser;
 import org.junit.Test;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.denimgroup.threadfix.CollectionUtils.map;
 
 /**
  * Created by mac on 6/11/14.
  */
 public class DotNetEndpointGeneratorTests {
 
-//    @Test
-//    public void testBasicEndpointGenerator() {
-//        DotNetRouteMappings routeMappings =
-//                DotNetRoutesParser.parse(ResourceManager.getDotNetMvcFile("RouteConfig.cs"));
-//        DotNetControllerMappings controllerMappings =
-//                DotNetControllerParser.parse(ResourceManager.getDotNetMvcFile("ChatController.cs")).get(0);
-//
-//        DotNetEndpointGenerator generator = new DotNetEndpointGenerator(null, routeMappings, null, new ArrayList<CSharpClass>(), controllerMappings);
-//
-//        assert generator.generateEndpoints().size() == 1 : "Size should have been 1 but was " +
-//                generator.generateEndpoints().size();
-//
-//        Endpoint endpoint = generator.generateEndpoints().get(0);
-//
-//        assert endpoint.getParameters().isEmpty() :
-//            "Parameters weren't empty. Got " + endpoint.getParameters();
-//        assert endpoint.getFilePath().endsWith("ChatController.cs") :
-//            "File path was " + endpoint.getFilePath() + " but should have ended with ChatController.cs";
-//        assert endpoint.getUrlPath().equals("/Chat") :
-//            "Actual path was " + endpoint.getUrlPath() + " when it should have been \"/Chat\"";
-//        assert endpoint.getStartingLineNumber() == 13 :
-//            "Starting line number was " + endpoint.getStartingLineNumber() + " but should have been 13.";
-//
-//    }
+    @Nonnull
+    private DotNetControllerMappings getMvcStandardControllerMappings(String fileName) {
+        List<CSharpClass> fileClasses = CSharpFileParser.parse(ResourceManager.getDotNetMvcFile(fileName));
+        Map<String, RouteParameterMap> fileParams = map(fileName, new RouteParameterMap());
+
+        CSharpClass mainClass = fileClasses.get(0);
+        if (!mainClass.getBaseTypes().contains("Controller")) {
+            mainClass.getBaseTypes().add("Controller");
+        }
+
+        AspActionGenerator actionGenerator = new AspStandardMvcActionGenerator(fileClasses, fileParams);
+        return actionGenerator.generate().get(0);
+    }
+
+    @Test
+    public void testBasicEndpointGenerator() {
+        DotNetRouteMappings routeMappings =
+                DotNetRoutesParser.parse(ResourceManager.getDotNetMvcFile("RouteConfig.cs"));
+        DotNetControllerMappings controllerMappings =
+                getMvcStandardControllerMappings("ChatController.cs");
+
+        DotNetEndpointGenerator generator = new DotNetEndpointGenerator(null, routeMappings, null, new ArrayList<CSharpClass>(), controllerMappings);
+
+        //  Should generate '/' and '/Chat'
+        assert generator.generateEndpoints().size() == 2 : "Size should have been 2 but was " +
+                generator.generateEndpoints().size();
+
+        Endpoint endpoint = generator.generateEndpoints().get(0);
+
+        assert endpoint.getParameters().isEmpty() :
+            "Parameters weren't empty. Got " + endpoint.getParameters();
+        assert endpoint.getFilePath().endsWith("ChatController.cs") :
+            "File path was " + endpoint.getFilePath() + " but should have ended with ChatController.cs";
+        assert endpoint.getUrlPath().equals("/Chat") :
+            "Actual path was " + endpoint.getUrlPath() + " when it should have been \"/Chat\"";
+        assert endpoint.getStartingLineNumber() == 13 :
+            "Starting line number was " + endpoint.getStartingLineNumber() + " but should have been 13.";
+
+    }
 
 
 }
